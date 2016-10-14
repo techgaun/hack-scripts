b525c2a9db32a40491020e326f53605920c5b906
techgaun <coolsamar207@gmail.com>
2016-10-14 02:32:56 -0500

    add pentest clone script

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 The ultimate goal is to create pipeline such that we can feed in some list of domains/urls and it goes through all the steps.
 
 - [massbanner.sh](massbanner.sh) - Specify file with list of URLs and get the server running
+- [setup-pendata.sh](setup-pendata.sh) - Clone popular and useful files and payloads without needing to go to each source
---
+++ setup-pendata.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# specify custom directory to install in as the first argument
+# eg. ./setup-pendata.sh /home/techgaun/pentest/files
+# defaults to $HOME/pentest
+
+ROOT_DIR=${1:-$HOME/pentest}
+PAYLOAD_DIR="${ROOT_DIR}/payloads"
+
+if [[ ! -e "${ROOT_DIR}" ]]; then
+  echo "${ROOT_DIR} does not exist. Creating..."
+  mkdir -p "${ROOT_DIR}"
+fi
+mkdir -p "${PAYLOAD_DIR}"
+
+# git clone https://github.com/danielmiessler/SecLists.git "${ROOT_DIR}/seclist"
+git clone https://github.com/nepalihackers/xss-payloads.git "${PAYLOAD_DIR}/xss"
+git clone https://github.com/xsuperbug/payloads.git "${PAYLOAD_DIR}/badfiles"
+git clone https://github.com/minimaxir/big-list-of-naughty-strings.git "${PAYLOAD_DIR}/badstrings"
