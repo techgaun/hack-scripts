5784ca0a318682d18fdce046b6fe7393c849a8a3
techgaun <coolsamar207@gmail.com>
2017-01-25 12:33:20 -0600

    add exif tools

--- setup-sys.sh
+++ setup-sys.sh
@@ -38,7 +38,8 @@ if is_debian; then
   apt install -y build-essential git vim silversearcher-ag axel aircrack-ng pyrit reaver wifite zenmap thc-ipv6 \
     nbtscan wireshark-qt tshark tcpdump vlan yersinia ettercap-text-only dsniff arp-scan ghex shutter whois \
     lft gnupg medusa hydra hydra-gtk libstrongswan p7zip-full forensics-all steghide dmitry ophcrack nginx-full \
-    socat swftools ruby-dev libpcap-dev php7.0-cli php7.0-fpm mutt git-email esmtp sysdig inotify-tools ack-grep
+    socat swftools ruby-dev libpcap-dev php7.0-cli php7.0-fpm mutt git-email esmtp sysdig inotify-tools ack-grep \
+    exif exifprobe
 elif is_rhel; then
   yum update
   yum groupinstall -y "Development Tools"
