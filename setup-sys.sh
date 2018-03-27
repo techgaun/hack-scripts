abeda09d94c4af285cb38619b742ce105b6f081c
techgaun <coolsamar207@gmail.com>
2018-03-26 22:00:03 -0500

    add aws ask cli

--- setup-sys.sh
+++ setup-sys.sh
@@ -97,7 +97,7 @@ wget -q -O - https://raw.githubusercontent.com/techgaun/extract/master/extract >
 wget -O ~/.bash_aliases https://raw.githubusercontent.com/techgaun/bash-aliases/master/.bash_aliases
 
 npm i -g diff-so-fancy apidoc flow-bin git+https://github.com/ramitos/jsctags.git castnow \
-  serverless
+  serverless ask-cli
 
 cd /tmp
 git clone https://github.com/facebook/watchman.git
