6897ad0888930c0456e2476da24a9bf64b2aa706
techgaun <coolsamar207@gmail.com>
2022-06-01 11:08:32 -0500

    gp openconnect gui

--- setup-sys.sh
+++ setup-sys.sh
@@ -183,3 +183,5 @@ curl -fsSL https://starship.rs/install.sh | bash
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
+
+sudo add-apt-repository -y ppa:yuezk/globalprotect-openconnect && sudo apt install globalprotect-openconnect
